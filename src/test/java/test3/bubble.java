1c4de7707c7075586a65c2b7aa12f188cce0de20
itcarswy <itsuperwww@163.com>
2021-01-29 09:13:08 +0800

    更新

---
+++ src/test/java/test3/bubble.java
@@ -0,0 +1,24 @@
+package test3;
+
+/**
+ * @Description 随机生成数组
+ * @Author Wy005
+ * @Date 2021/1/29 9:00
+ * @Version 1.0
+ **/
+public class bubble {
+    public static void main(String[] args) {
+        int []arr=gennerateArray(10, 100);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+"-");
+        }
+    }
+    public static int[]  gennerateArray(int len,int max){
+        int[] arr=new int[len];
+        for(int i=0;i<arr.length;i++){
+            arr[i]=(int)(Math.random()*max);
+        }
+        return arr;
+    }
+
+}
